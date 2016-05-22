597906b80be12c5e2be717741d358b4d89e1bdd1
Patrick Cullen <patrick.cullen@washpost.com>
2016-05-21 21:50:02 -0400

    added datadog monitors

--- cloud-compose/templates/datadog.docker.sh
+++ cloud-compose/templates/datadog.docker.sh
@@ -1,3 +1,5 @@
 # datadog.docker.sh 
+{%- if DATADOG_API_KEY is defined %}
 sh -c "sed 's/api_key:.*/api_key: {{DATADOG_API_KEY}}/' /etc/dd-agent/datadog.conf.example > /etc/dd-agent/datadog.conf"
 service datadog-agent restart
+{%- endif %}
