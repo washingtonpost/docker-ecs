efe672cea3b6ca540d600ce53b71a71b8c74c82b
Patrick Cullen <patrick.cullen@washpost.com>
2016-05-26 18:32:19 -0400

    made datadog required

--- cloud-compose/templates/datadog.docker.sh
+++ cloud-compose/templates/datadog.docker.sh
@@ -1,5 +1,3 @@
-{%- if DATADOG_API_KEY is defined %}
 # datadog.docker.sh 
 sh -c "sed 's/api_key:.*/api_key: {{DATADOG_API_KEY}}/' /etc/dd-agent/datadog.conf.example > /etc/dd-agent/datadog.conf"
 service datadog-agent restart
-{%- endif %}
