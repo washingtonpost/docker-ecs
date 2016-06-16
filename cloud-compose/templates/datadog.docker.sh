ac01663b3db0da64fef77a67202d23c12ba0c3ef
Patrick Cullen <patrick.cullen@washpost.com>
2016-06-16 19:06:03 -0400

    fixed script typo

--- cloud-compose/templates/datadog.docker.sh
+++ cloud-compose/templates/datadog.docker.sh
@@ -1,5 +1,5 @@
 # datadog.docker.sh 
 sh -c "sed 's/api_key:.*/api_key: {{DATADOG_API_KEY}}/' /etc/dd-agent/datadog.conf.example > /etc/dd-agent/datadog.conf"
 # Note 'bind_host: 0.0.0.0' will break the built in JMX collector on the host, but it is required for sending metrics directly from Docker containers to the dd-agent
-'bind_host: 0.0.0.0' >> /etc/dd-agent/datadog.conf
+echo 'bind_host: 0.0.0.0' >> /etc/dd-agent/datadog.conf
 service datadog-agent restart
