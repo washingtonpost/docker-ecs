1151c0b2039edc1852cb99a9c084de53418dc162
Patrick Cullen <patrick.cullen@washpost.com>
2017-01-28 07:08:47 -0500

    added kms encrypted secrets

--- cloud-compose/templates/cluster.sh
+++ cloud-compose/templates/cluster.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 {% include "system.limits_conf.sh" %}
 {% include "cloud.environment.sh" %}
+{% include "secrets.environment.sh" %}
 {% include "system.mounts.sh" %}
 {% include "docker.config.sh" %}
 {% include "docker_compose.run.sh" %}
