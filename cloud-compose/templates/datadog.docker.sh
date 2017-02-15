182625414a1cde2b17fbff4cc4252bce17e923de
Chris Nguyen <christopher.nguyen@washpost.com>
2017-02-15 12:51:12 -0500

    Adds support for using DATADOG_API_KEY defined in secrets
    If DATADOG_API_KEY is defined in cloud-compose.yml secrets, set
    the datadog configuration to use that instead of a local
    environment variable passed to the template.

--- cloud-compose/templates/datadog.docker.sh
+++ cloud-compose/templates/datadog.docker.sh
@@ -1,5 +1,9 @@
-# datadog.docker.sh 
+# datadog.docker.sh
+{%- if secrets.DATADOG_API_KEY is defined %}
+sh -c "sed 's/api_key:.*/api_key: $DATADOG_API_KEY/' /etc/dd-agent/datadog.conf.example > /etc/dd-agent/datadog.conf"
+{%- else %}
 sh -c "sed 's/api_key:.*/api_key: {{DATADOG_API_KEY}}/' /etc/dd-agent/datadog.conf.example > /etc/dd-agent/datadog.conf"
+{%- endif %}
 # Note 'bind_host: 0.0.0.0' will break the built in JMX collector on the host, but it is required for sending metrics directly from Docker containers to the dd-agent
 echo 'bind_host: 0.0.0.0' >> /etc/dd-agent/datadog.conf
 service datadog-agent restart
