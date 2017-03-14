0f914ed43d9f190c02a60d3fa0ff27e9c68351ee
Findley, Randy <rgfindley@gmail.com>
2017-03-14 10:14:17 -0400

    Fix scripts so cloud-compose.yml 'environment' vars are included.

--- cloud-compose/templates/environment.sh
+++ cloud-compose/templates/environment.sh
@@ -1,7 +1,15 @@
-# secrets.environment.sh
+# environment.sh
+# Secrets
 {%- if secrets is defined %}
 AWS_REGION="{{ AWS_REGION }}"
 {%- for key, val in secrets.iteritems() %}
 {{ key }}=$(aws kms decrypt --region $AWS_REGION --ciphertext-blob fileb://<(echo '{{ val }}' | base64 -d) --output text | cut -f 2 -d $'\t' | base64 -d)
+{%- endfor %}
+{%- endif %}
+
+# Environment
+{%- if environment is defined %}
+{%- for key, val in environment.iteritems() %}
+{{ key }}={{ val }}
 {%- endfor %}
 {%- endif %}
