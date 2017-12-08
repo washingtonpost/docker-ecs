bce146d75a10c5973fa57410ac302baa11b2480a
Mason Foster <mason.foster@washpost.com>
2017-12-08 11:56:37 -0500

    Fix datadog configuration options

--- cloud-compose/templates/datadog.docker.sh
+++ cloud-compose/templates/datadog.docker.sh
@@ -5,6 +5,7 @@ sh -c "sed 's/api_key:.*/api_key: $DATADOG_API_KEY/' /etc/dd-agent/datadog.conf.
 sh -c "sed 's/api_key:.*/api_key: {{DATADOG_API_KEY}}/' /etc/dd-agent/datadog.conf.example > /etc/dd-agent/datadog.conf"
 {%- endif %}
 # Note 'bind_host: 0.0.0.0' will break the built in JMX collector on the host, but it is required for sending metrics directly from Docker containers to the dd-agent
-echo 'bind_host: 0.0.0.0' >> /etc/dd-agent/datadog.conf
-echo 'process_agent_enabled: true' >> /etc/dd-agent/datadog.conf
+sh -c "sed -i 's/# bind_host:.*/bind_host: 0.0.0.0/' /etc/dd-agent/datadog.conf"
+sh -c "sed -i '/\[Main\]/aprocess_agent_enabled: true' /etc/dd-agent/datadog.conf"
+
 service datadog-agent restart
