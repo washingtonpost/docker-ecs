447f6164cea2c6c430e5838317348c56ddb3af77
Julien Lancien <julien.lancien@washpost.com>
2018-01-19 15:13:46 -0500

    Enable APM by default

--- cloud-compose/templates/datadog.docker.sh
+++ cloud-compose/templates/datadog.docker.sh
@@ -6,6 +6,7 @@ sh -c "sed 's/api_key:.*/api_key: {{DATADOG_API_KEY}}/' /etc/dd-agent/datadog.co
 {%- endif %}
 # Note 'bind_host: 0.0.0.0' will break the built in JMX collector on the host, but it is required for sending metrics directly from Docker containers to the dd-agent
 sh -c "sed -i 's/# bind_host:.*/bind_host: 0.0.0.0/' /etc/dd-agent/datadog.conf"
+sh -c "sed -i '/\[Main\]/aapm_enabled: true' /etc/dd-agent/datadog.conf"
 sh -c "sed -i '/\[Main\]/aprocess_agent_enabled: true' /etc/dd-agent/datadog.conf"
 
 service datadog-agent restart
